011d7481edfc154380bc18360ead5c5a96da86bc
rasyidcode <jamil@dwebsite.net>
2021-07-05 01:15:06 +0800

    starting to write chatwheel_data_source

--- lib/data/network/chatwheel_data_source.dart
+++ lib/data/network/chatwheel_data_source.dart
@@ -1,4 +1,8 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:flutter_dota_2_chatwheel/data/model/chatwheel_event.dart';
+import 'package:flutter_dota_2_chatwheel/data/model/chatwheel_event_result.dart';
 import 'package:flutter_dota_2_chatwheel/data/model/chatwheel_pack.dart';
+import 'package:flutter_dota_2_chatwheel/data/scraper/chatwheel_scraper.dart';
 import 'package:http/http.dart' as http;
 
 const EVENT_CHATWHEEL_URL = 'https://dota2.fandom.com/wiki/Chat_Wheel';
@@ -9,23 +13,31 @@ const SUPPORTERS_CLUB_CHATWHEEL_URL =
 
 class ChatwheelDataSource {
   final http.Client _client;
+  final ChatwheelScraper _scraper;
 
-  ChatwheelDataSource(this._client);
+  ChatwheelDataSource(this._client, this._scraper);
 
-//   Future<List<Chatwheel>> getAllChatwheels() async {
-//     final chatwheelPageResponse = await _client.get(Uri.parse(CHATWHEEL_URL));
+  Future<BuiltList<ChatwheelEvent?>> getChatwheelEvents() async {
+    final chatwheelPageResponse =
+        await _client.get(Uri.parse(EVENT_CHATWHEEL_URL));
 
-//     if (chatwheelPageResponse.statusCode == 200) {
-//       final chatwheelPageDocument = parse(chatwheelPageResponse.body);
-//       final
-//       return <Chatwheel>[];
-//     } else {
-//       throw UnhandledException();
-//     }
-//   }
-// }
+    if (chatwheelPageResponse.statusCode == 200) {
+      final chatwheelEventResult =
+          _scraper.getEvents2(chatwheelPageResponse.body);
 
-// class UnhandledException implements Exception {
-//   final String message = 'Something went wrong';
-// }
+      if (chatwheelEventResult.events.length == 0) throw EmptyResultException();
+
+      return chatwheelEventResult.events;
+    } else {
+      throw UnhandledException();
+    }
+  }
+}
+
+class UnhandledException implements Exception {
+  final String message = 'Something went wrong';
+}
+
+class EmptyResultException implements Exception {
+  final String message = 'No chatwheels found';
 }
--- test/data/scraper/chatwheel_scraper_test.dart
+++ test/data/scraper/chatwheel_scraper_test.dart
@@ -16,8 +16,8 @@ void main() {
           .readAsStringSync();
   final chatwheelScraper = ChatwheelScraper();
 
-  group('chatwheel scrapper test | ', () {
-    test('returns ChatwheelLine and check it\'s properties', () {
+  group('chatwheel_scraper.dart test', () {
+    test('should return ChatwheelLine model and correct values', () {
       String sampleDocumentString = fixtures('the_correct_span');
       String sampleDocumentString2 = fixtures('the_correct_span2');
 
@@ -41,102 +41,9 @@ void main() {
       expect(line2.lineTranslate, 'God, look around at what is happening!');
       expect(line2.url,
           'https://static.wikia.nocookie.net/dota2_gamepedia/images/6/6a/Misc_soundboard_bozhe_ti_posmotri.mp3/revision/latest?cb=20190922222030');
-
-      // print(line);
-      // print(line2);
     });
 
-    test('returns ChatwheelPack and check it\'s properties', () {
-      String sampleDocumentString = fixtures('pack_element');
-      String sampleDocumentString2 = fixtures('pack_element2');
-
-      final Document sampleDocument = parse(sampleDocumentString);
-      final Document sampleDocument2 = parse(sampleDocumentString2);
-
-      final Element? span = sampleDocument.querySelector('li > span > audio');
-      final Element? span2 = sampleDocument2.querySelector('li > span > audio');
-
-      final packs = chatwheelScraper.getPack(span);
-      final linesMatcher = BuiltList<ChatwheelLine>([
-        ChatwheelLine((b) => b
-          ..line = 'Ba-dum tishh'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/f/fc/Misc_soundboard_rimshot.mp3/revision/latest?cb=20190922222137'),
-        ChatwheelLine((b) => b
-          ..line = 'Charge'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/d/d6/Misc_soundboard_charge.mp3/revision/latest?cb=20190922222034'),
-        ChatwheelLine((b) => b
-          ..line = 'Frog'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/c/c9/Misc_soundboard_frog.mp3/revision/latest?cb=20190922222046'),
-        ChatwheelLine((b) => b
-          ..line = 'Crash and burn'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/b/ba/Misc_soundboard_crash_burn.mp3/revision/latest?cb=20190922222034'),
-        ChatwheelLine((b) => b
-          ..line = 'Applause'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/a/a4/Misc_crowd_lv_01.mp3/revision/latest?cb=20190922221550'),
-        ChatwheelLine((b) => b
-          ..line = 'Applause'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/5/54/Misc_crowd_lv_02.mp3/revision/latest?cb=20190922221551'),
-      ]);
-
-      final packs2 = chatwheelScraper.getPack(span2);
-      final linesMatcher2 = BuiltList<ChatwheelLine>([
-        ChatwheelLine((b) => b
-          ..line = 'Ba-dum tishh'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/f/fc/Misc_soundboard_rimshot.mp3/revision/latest?cb=20190922222137'),
-        ChatwheelLine((b) => b
-          ..line = 'Charge'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/d/d6/Misc_soundboard_charge.mp3/revision/latest?cb=20190922222034'),
-        ChatwheelLine((b) => b
-          ..line = 'Frog'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/c/c9/Misc_soundboard_frog.mp3/revision/latest?cb=20190922222046'),
-        ChatwheelLine((b) => b
-          ..line = 'Crash and burn'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/b/ba/Misc_soundboard_crash_burn.mp3/revision/latest?cb=20190922222034'),
-        ChatwheelLine((b) => b
-          ..line = 'Applause'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/a/a4/Misc_crowd_lv_01.mp3/revision/latest?cb=20190922221550'),
-        ChatwheelLine((b) => b
-          ..line = 'Applause'
-          ..url =
-              'https://static.wikia.nocookie.net/dota2_gamepedia/images/5/54/Misc_crowd_lv_02.mp3/revision/latest?cb=20190922221551'),
-      ]);
-
-      expect(packs, TypeMatcher<ChatwheelPack>());
-
-      expect(packs.bpLevel, TypeMatcher<int>());
-      expect(packs.bpLevel, 26);
-
-      expect(packs.packName, TypeMatcher<String>());
-      expect(packs.packName, 'bp26');
-
-      expect(packs.lines, TypeMatcher<BuiltList<ChatwheelLine>>());
-      expect(packs.lines.length, linesMatcher.length);
-      expect(packs.lines, linesMatcher);
-
-      expect(packs2, TypeMatcher<ChatwheelPack>());
-      expect(packs2.bpLevel, TypeMatcher<int>());
-      expect(packs2.bpLevel, 24);
-
-      expect(packs2.packName, TypeMatcher<String>());
-      expect(packs2.packName, 'Sound Pack 1 (2017)');
-
-      expect(packs2.lines, TypeMatcher<BuiltList<ChatwheelLine>>());
-      expect(packs2.lines.length, linesMatcher2.length);
-      expect(packs2.lines, linesMatcher2);
-    });
-
-    test('test getPack2()', () {
+    test('should return ChatwheelPack model and correct values', () {
       String sampleDocumentString = fixtures('pack_element');
       String sampleDocumentString2 = fixtures('pack_element2');
       String sampleDocumentString3 = fixtures('pack_element3');
@@ -161,21 +68,9 @@ void main() {
       expect(packs2, TypeMatcher<ChatwheelPack>());
       expect(packs3, TypeMatcher<ChatwheelPack>());
       expect(packs4, TypeMatcher<ChatwheelPack>());
-
-      print(packs4);
     });
 
-    test('returns List of ChatwheelPack and check it\'s properties', () {
-      String sampleDocumentString = fixtures('event_element');
-
-      final Document sampleDocument = parse(sampleDocumentString);
-      final Element? span = sampleDocument.querySelector(
-          'table.wikitable.sortable > tbody > tr > td > ul > li > span > audio');
-      final event = chatwheelScraper.getEvent(span);
-      expect(event, TypeMatcher<ChatwheelEvent>());
-    });
-
-    test('test getEvent2', () {
+    test('should return either ChatwheelEvent or null value', () {
       String sampleDocumentString = fixtures('event_element');
       String sampleDocumentString2 = fixtures('event_element2');
       String sampleDocumentString3 = fixtures('event_element3');
@@ -197,17 +92,10 @@ void main() {
       expect(event3, null);
     });
 
-    test('returns ChatwheelEventResult', () {
-      String sampleDocumentString = fixtures('chatwheel_fullpage');
-      final events = chatwheelScraper.getEvents(sampleDocumentString);
-      expect(events, TypeMatcher<ChatwheelEventResult>());
-    });
-
-    test('test getEvent2()', () {
+    test('should return a ChatwheelEventResult', () {
       String sampleDocumentString = fixtures('chatwheel_fullpage2');
       final events = chatwheelScraper.getEvents2(sampleDocumentString);
       expect(events, TypeMatcher<ChatwheelEventResult>());
-      print(events.events.length);
     });
   });
 }
